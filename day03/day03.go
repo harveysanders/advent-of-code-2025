f93917607de8835e585555070141f2d836cf9356
Harvey Sanders <sanders.harvey@gmail.com>
2025-12-03 19:35:51 -0500

    First pass on day3,part 2

--- day03/day03.go
+++ day03/day03.go
@@ -3,6 +3,7 @@ package day03
 import (
 	"bytes"
 	"io"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -37,33 +38,46 @@ func ParseBanks(in io.Reader) (Banks, error) {
 	return banks, nil
 }
 
-func (b Bank) FindHighestJoltage() int {
-	var max1stDigit int
-	var max2ndDigit int
-	var maxPos int
-
-	// Skip the last digit because we need at least 2 batteries
-	for i := 0; i < len(b.Batteries)-1; i++ {
-		curJ := b.Batteries[i]
-		if curJ > max1stDigit {
-			max1stDigit = curJ
-			maxPos = i
+func (b Bank) FindHighestJoltage(n int) int {
+	var recurse func([]int, []int, int) []int
+	recurse = func(restBatts []int, joltages []int, nLeft int) []int {
+		if nLeft == 0 {
+			return joltages
 		}
-	}
-	// slice the batteries list from the max1stDigit to end
-	for _, v := range b.Batteries[maxPos+1:] {
-		if v > max2ndDigit {
-			max2ndDigit = v
+		var maxPos int
+		var max int
+		// TODO: there are diminishing returns if we go too far to the
+		// right, because there are not enough choices left to iterate through
+		// figure out the pattern
+		for i, v := range restBatts {
+			// Skip the last digit because we need at least 2 batteries
+			if len(joltages) == 0 && i == len(restBatts)-1 {
+				continue
+			}
+			if v > max {
+				max = v
+				maxPos = i
+			}
 		}
+		next := append(joltages, max)
+		return recurse(restBatts[maxPos+1:], next, nLeft-1)
 	}
 
-	return (max1stDigit * 10) + max2ndDigit
+	joltages := make([]int, 0, n)
+	joltages = recurse(b.Batteries, joltages, n)
+	var total float64
+	for i, v := range joltages {
+		place := math.Pow(10, float64(n-i-1))
+		total += float64(v) * place
+	}
+
+	return int(total)
 }
 
-func (b Banks) Total() int {
+func (b Banks) Total(n int) int {
 	var total int
 	for _, bank := range b {
-		total += bank.FindHighestJoltage()
+		total += bank.FindHighestJoltage(n)
 	}
 	return total
 }
--- day03/day03_test.go
+++ day03/day03_test.go
@@ -13,15 +13,15 @@ import (
 //go:embed input/*.txt
 var inputFS embed.FS
 
-func TestPart1(t *testing.T) {
+func TestTotal(t *testing.T) {
 	testCases := []struct {
 		desc      string
 		getInput  func() (io.Reader, error)
+		nDigits   int
 		wantTotal int
 	}{
-
 		{
-			desc: "example",
+			desc: "example - part 1",
 			getInput: func() (io.Reader, error) {
 				in := strings.NewReader(`
 987654321111111
@@ -31,10 +31,25 @@ func TestPart1(t *testing.T) {
 `[1:])
 				return in, nil
 			},
+			nDigits:   2,
 			wantTotal: 357,
 		},
 		{
-			desc: "real input",
+			desc: "example - part 2",
+			getInput: func() (io.Reader, error) {
+				in := strings.NewReader(`
+987654321111111
+811111111111119
+234234234234278
+818181911112111
+`[1:])
+				return in, nil
+			},
+			nDigits:   12,
+			wantTotal: 3121910778619,
+		},
+		{
+			desc: "real input - part 1",
 			getInput: func() (io.Reader, error) {
 				f, err := inputFS.Open("input/input.txt")
 				if err != nil {
@@ -42,6 +57,19 @@ func TestPart1(t *testing.T) {
 				}
 				return f, nil
 			},
+			nDigits:   2,
+			wantTotal: 17095,
+		},
+		{
+			desc: "real input - part 2",
+			getInput: func() (io.Reader, error) {
+				f, err := inputFS.Open("input/input.txt")
+				if err != nil {
+					return nil, err
+				}
+				return f, nil
+			},
+			nDigits:   12,
 			wantTotal: 17095,
 		},
 	}
@@ -54,7 +82,7 @@ func TestPart1(t *testing.T) {
 			banks, err := day03.ParseBanks(input)
 			require.NoError(t, err)
 
-			gotTotal := banks.Total()
+			gotTotal := banks.Total(tc.nDigits)
 			require.Equal(t, tc.wantTotal, gotTotal)
 		})
 	}
@@ -64,41 +92,78 @@ func TestFindHighestJoltage(t *testing.T) {
 	testCases := []struct {
 		desc        string
 		bank        day03.Bank
+		nDigits     int
 		wantJoltage int
 	}{
 		{
-			desc: "987654321111111",
+			desc: "part 1: 987654321111111",
 			bank: day03.Bank{
 				Batteries: []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1},
 			},
+			nDigits:     2,
 			wantJoltage: 98,
 		},
 		{
-			desc: "811111111111119",
+			desc: "part 1: 811111111111119",
 			bank: day03.Bank{
 				Batteries: []int{8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9},
 			},
+			nDigits:     2,
 			wantJoltage: 89,
 		},
 		{
-			desc: "234234234234278",
+			desc: "part 1: 234234234234278",
 			bank: day03.Bank{
 				Batteries: []int{2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8},
 			},
+			nDigits:     2,
 			wantJoltage: 78,
 		},
 		{
-			desc: "818181911112111",
+			desc: "part 1: 818181911112111",
 			bank: day03.Bank{
 				Batteries: []int{8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1},
 			},
+			nDigits:     2,
 			wantJoltage: 92,
+		},
+		{
+			desc: "part 2: 987654321111111",
+			bank: day03.Bank{
+				Batteries: []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1},
+			},
+			nDigits:     12,
+			wantJoltage: 987654321111,
+		},
+		{
+			desc: "part 2: 811111111111119",
+			bank: day03.Bank{
+				Batteries: []int{8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9},
+			},
+			nDigits:     12,
+			wantJoltage: 811111111119,
+		},
+		{
+			desc: "part 2: 234234234234278",
+			bank: day03.Bank{
+				Batteries: []int{2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8},
+			},
+			nDigits:     12,
+			wantJoltage: 434234234278,
+		},
+		{
+			desc: "part 2: 818181911112111",
+			bank: day03.Bank{
+				Batteries: []int{8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1},
+			},
+			nDigits:     12,
+			wantJoltage: 888911112111,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
-			got := tc.bank.FindHighestJoltage()
+			got := tc.bank.FindHighestJoltage(tc.nDigits)
 			require.Equal(t, tc.wantJoltage, got)
 		})
 	}
