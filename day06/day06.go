a0fc14de49f5e2fbb534556cd5b472e5fd84e35b
Harvey Sanders <sanders.harvey@gmail.com>
2025-12-03 20:24:17 -0500

    day 6 part 2

--- day06/day06.go
+++ day06/day06.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"slices"
 	"strconv"
 	"strings"
 	"sync"
@@ -20,6 +19,23 @@ const (
 type Problem struct {
 	op          operator
 	rawOperands chan string
+	// The following fields are only used for vertial numDir.
+
+	// For vertically aligned numbers, the offset from the start
+	// of the row to the first column of the problem's
+	// column group.
+	// Ex:
+	//   123 328  51 64
+	//    45 64  387 23
+	//     6 98  215 314
+	//   *   +   *   +
+	// in the worksheet above, the leftmost problem's offset is 0
+	// determined by the operator position.
+	// The next problem, from left to right, has an offset of 4
+	offset int
+	// Number of colums the problem uses.
+	colWidth int
+	rawRows  []string
 }
 
 type Worksheet struct {
@@ -35,24 +51,45 @@ func ParseWorksheet(r io.Reader) (Worksheet, error) {
 	raw = bytes.TrimSuffix(raw, []byte("\n"))
 	lines := strings.Split(string(raw), "\n")
 
-	operators := strings.Fields(lines[len(lines)-1])
-
 	w := Worksheet{
-		probs:   make([]Problem, 0, len(operators)),
+		probs:   make([]Problem, 0, 200),
 		rawRows: lines[:len(lines)-1],
 	}
-	validOps := []operator{OpAdd, OpMultiply}
-	for _, op := range operators {
-		if !slices.Contains(validOps, operator(op)) {
-			return w, fmt.Errorf("invalid operator: %q", op)
-		}
-		prob := Problem{
-			op:          operator(op),
-			rawOperands: make(chan string, 1),
-		}
 
-		w.probs = append(w.probs, prob)
+	operatorLine := lines[len(lines)-1]
+	var prob Problem
+	for col, v := range strings.Split(operatorLine, "") {
+		switch v {
+		case string(OpAdd), string(OpMultiply):
+			// stop parsing the prev problem
+			// and add to list
+			if col > 0 {
+				// decrement the col width for
+				// the separator column
+				prob.colWidth -= 1
+				w.probs = append(w.probs, prob)
+			}
+			// --------------------
+			// start a new problem
+			prob = Problem{
+				rawOperands: make(chan string, 1),
+				offset:      col,
+				rawRows:     w.rawRows,
+				colWidth:    1,
+			}
+			switch operator(v) {
+			case OpAdd:
+				prob.op = OpAdd
+			case OpMultiply:
+				prob.op = OpMultiply
+			}
+			continue
+		case " ":
+			prob.colWidth += 1
+		}
 	}
+	// add the last problem
+	w.probs = append(w.probs, prob)
 
 	return w, nil
 }
@@ -106,4 +143,64 @@ func (p Problem) calc() int {
 		}
 	}
 	return total
+}
+
+func (w Worksheet) Do2() int {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var total int
+
+	for _, prob := range w.probs {
+		wg.Go(func() {
+			res, err := prob.calc2()
+			if err != nil {
+				panic(err)
+			}
+			mu.Lock()
+			total += res
+			mu.Unlock()
+		})
+	}
+
+	wg.Wait()
+	return total
+}
+
+func (p Problem) calc2() (int, error) {
+	var total int
+	if p.op == OpMultiply {
+		// so we don't multiply by 0
+		total = 1
+	}
+	operands := make([]int, 0, len(p.rawRows))
+	var rawNum strings.Builder
+	for x := p.offset; x < p.colWidth+p.offset; x++ {
+		for y := range p.rawRows {
+			char := p.rawRows[y][x]
+			if char == ' ' {
+				continue
+			}
+			err := rawNum.WriteByte(char)
+			if err != nil {
+				return 0, err
+			}
+		}
+		str := rawNum.String()
+		num, err := strconv.Atoi(str)
+		if err != nil {
+			return 0, err
+		}
+		operands = append(operands, num)
+		rawNum.Reset()
+	}
+
+	for _, v := range operands {
+		switch p.op {
+		case OpAdd:
+			total += v
+		case OpMultiply:
+			total *= v
+		}
+	}
+	return total, nil
 }
--- day06/day06_test.go
+++ day06/day06_test.go
@@ -25,7 +25,7 @@ func TestPart1(t *testing.T) {
 123 328  51 64 
  45 64  387 23 
   6 98  215 314
-*   +   *   + 
+*   +   *   +  
 `[1:])
 				return in, nil
 			},
@@ -40,7 +40,7 @@ func TestPart1(t *testing.T) {
 				}
 				return f, nil
 			},
-			wantTotal: 0,
+			wantTotal: 5667835681547,
 		},
 	}
 	for _, tc := range testCases {
@@ -53,6 +53,52 @@ func TestPart1(t *testing.T) {
 			require.NoError(t, err)
 
 			total := worksheet.Do()
+			require.Equal(t, tc.wantTotal, total)
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	testCases := []struct {
+		desc      string
+		getInput  func() (io.Reader, error)
+		wantTotal int
+	}{
+		{
+			desc: "example - part 2",
+			getInput: func() (io.Reader, error) {
+				in := strings.NewReader(`
+123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  
+`[1:])
+				return in, nil
+			},
+			wantTotal: 3263827,
+		},
+		{
+			desc: "real input - part 2",
+			getInput: func() (io.Reader, error) {
+				f, err := inputFS.Open("input/input.txt")
+				if err != nil {
+					return nil, err
+				}
+				return f, nil
+			},
+			wantTotal: 9434900032651,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+
+			input, err := tc.getInput()
+			require.NoError(t, err)
+
+			worksheet, err := ParseWorksheet(input)
+			require.NoError(t, err)
+
+			total := worksheet.Do2()
 			require.Equal(t, tc.wantTotal, total)
 		})
 	}
